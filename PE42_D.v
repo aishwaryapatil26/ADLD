da9ecc9111e464d7d2f1bea51e46950b1b2d9845
aishwaryapatil26 <105833505+aishwaryapatil26@users.noreply.github.com>
2022-05-20 21:21:38 +0530

    Add files via upload

---
+++ PE42_D.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:17:22 04/29/2022 
+// Design Name: 
+// Module Name:    PE42_D 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module PE42_D(A0,A1,Y0,Y1,Y2,Y3);
+input Y0,Y1,Y2,Y3;
+output A0,A1;
+assign A1=Y3+Y2;
+assign A0=Y3+Y1;
+endmodule
+
+
