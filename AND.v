c89f312076065d9d8c23c08ec7be64921f15a28a
aishwaryapatil26 <105833505+aishwaryapatil26@users.noreply.github.com>
2022-05-20 21:06:27 +0530

    Add files via upload

---
+++ AND.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:02:39 04/29/2022 
+// Design Name: 
+// Module Name:    AND 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module AND(
+input a,
+input b,
+output y
+ );
+assign y=a&b;
+endmodule
+
