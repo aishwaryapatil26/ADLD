5b6af5fdab35955bbfa68b78b9a07a80349f263a
aishwaryapatil26 <105833505+aishwaryapatil26@users.noreply.github.com>
2022-05-20 20:33:10 +0530

    Add files via upload

---
+++ parking_system_tb.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:07:50 04/07/2022 
+// Design Name: 
+// Module Name:    parking_system_tb 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module parking_system_tb(
+    );
+
+
+endmodule
