21c67a3eb1d5db4627bb1bdb36662f2c0f50737d
aishwaryapatil26 <105833505+aishwaryapatil26@users.noreply.github.com>
2022-05-20 20:46:32 +0530

    Add files via upload

---
+++ flipflop.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    09:59:16 04/18/2022 
+// Design Name: 
+// Module Name:    flipflop 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module flipflop(
+    );
+
+
+endmodule
